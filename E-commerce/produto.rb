2d4814dfbdc8e6bc9a0b574717a5e11e4a01314c
Leonardo Sanches Alves <leonarsanalves@gmail.com>
2025-09-19 16:35:24 -0300

    Atualizando código

--- E-commerce/produto.rb
+++ E-commerce/produto.rb
@@ -1,27 +1,91 @@
 class Product
 
-    attr_accessor :name, :value,:id,:type
+    attr_accessor :name, :value,:id,:type, :category
 
 
-    def initialize (name,value,id,type)
+    def initialize (name,value,id,type,category,stock=[])
         @name = name
         @value = value
         @id = id
         @type = type
+        @category = category
+        @stock = stock
     end
 
         def use
 
-            if type == 'summer'
+            if type == 'parte de baixo'
 
                 pp "#{name}, posso usar no verao"
             end 
         end
 end
 
-    product1 = Product.new 't-shirt','40$','1','summer'
-    product2 = Product.new 'jeans','50$','2','various'
-    product3 = Product.new 'shorts','20$','3','summer'
+    product1 = Product.new't-shirt','40$','1','parte de cima','roupa',4
+    product2 = Product.new 'jeans','50$','2','parte de baixo','roupa',4
+    product3 = Product.new 'shorts','20$','3','parte de baixo','roupa',4
 
 
 pp product3.use
+
+
+
+
+# Desafio 4
+
+
+# Método comprar produto / Vender produto -> loja
+
+# Método verificar estoque -> loja
+
+# e um método pra verificar o stock
+
+
+
+class Store
+
+    attr_accessor :name, :id, :catalogo
+
+
+    def initialize (name,id,catalogo=[])
+        
+        @name = name
+        @id = id
+        @catalogo = catalogo
+        
+    end 
+
+    def add_product(product)
+        @catalogo << product
+    end 
+
+
+    def buy_product(product)
+        @catalogo.delete(product)
+    end
+
+
+    def stock_store(products)
+        @catalogo.count(products)
+    end 
+
+    def remove_product(product)
+        @catalogo.delete(product)
+    end
+end
+
+
+store01 = Store.new("loja sanches",01,[product3])
+
+
+store01.add_product(product1)
+store01.add_product(product2)
+
+pp store01.catalogo
+
+
+store01.remove_product(product1)
+
+# stock01.buy_product(@stock)
+
+pp store01.catalogo
--- README.md
+++ README.md
@@ -5,3 +5,50 @@ Produto deve ter um nome
 Produto deve ter um valor
 Produto deve ter um id
 Produto deve ter um tipo
+
+
+Passo 2
+
+
+Agora quero que tu crie a classe loja
+ 
+ela tem nome, id e produtos
+ 
+quero um método que adiciona mais produtos
+ 
+e um que remove produto
+ 
+o atributo produto é um array de produtos
+ 
+
+
+ Desafio 3
+
+
+Ao invés dos products serem strings
+ 
+quero que ele seja um Array de products (Classe Product)
+ 
+ou seja no momento da instancia da classe Loja, você pode receber um array de Product
+
+
+Desafio 4
+
+
+Método comprar produto / Vender produto -> loja
+Método verificar estoque -> loja
+Atributos em produto = categoria / mudar o type 
+ 
+
+
+
+# Método comprar produto / Vender produto -> loja
+# Método verificar estoque -> loja
+#eu ai falar pra tu adicionar a propriedade stock na classe product
+#  Mas agora então vou focar e deixar método remove e "vende"
+# e adicionar o stock
+# e um método pra verificar o stock
+# e alterar o nome de product pra catalogo
+
+
+# Atributos em produto = categoria / mudar o type 
